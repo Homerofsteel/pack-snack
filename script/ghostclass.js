67504427c42404610e6995da6a5b455fe05cc388
homerofsteel <lucas.vernageau@ynov.com>
2025-03-27 10:49:18 +0100

    WIP ghostclass

--- script/ghostclass.js
+++ script/ghostclass.js
@@ -1,52 +1,59 @@
 class Ghost {
     constructor(elementId, position, speed) {
         this.ghost = document.getElementById(elementId);
-        this.position = position;
-        this.speed = speed;
+        this.position = position ;
+        this.speed = speed ;
         this.direction = null;
-        this.interval = setInterval(() => this.move(), 30); 
 
         this.ghost.style.position = "absolute";
         this.ghost.style.left = this.position.x + "px";
         this.ghost.style.top = this.position.y + "px";
 
-        this.init();
+        this.startMoving();
     }
 
+    startMoving() {
+        
+        this.setRandomDirection();
+        this.move();
+    }
 
-    move() {
+    setRandomDirection() {
+        const directions = [
+            { x: this.speed.x, y: 0 },  
+            { x: -this.speed.x, y: 0 }, 
+            { x: 0, y: this.speed.y }, 
+            { x: 0, y: -this.speed.y }  
+        ];
+        this.direction = directions[Math.floor(Math.random() * directions.length)];
+    }
 
-        let left = parseInt(this.ghost.style.left);
-        let top = parseInt(this.ghost.style.top);
+    move = () => {
 
-        let nextLeft = left;
-        let nextTop = top;
+        let nextX = this.position.x + this.direction.x;
+        let nextY = this.position.y + this.direction.y;
 
-        let randomnumber=Math.random(1,4)
-
-        setTimeout(function () {
-
-        if (randomnumber==1) {
-            nextLeft += this.speed.x;
-        } else if (randomnumber==2) {
-            nextLeft -= this.speed.x;
-        } else if (randomnumber==3) {
-            nextTop -= this.speed.y;
-        } else if (randomnumber==4) {
-            nextTop += this.speed.y;
+        if (nextX < 0 || nextX > window.innerWidth - this.ghost.offsetWidth ||
+            nextY < 0 || nextY > window.innerHeight - this.ghost.offsetHeight) {
+            this.setRandomDirection();
+            if (
+                nextX < ghost1.position.x + ghost1.ghost.offsetWidth &&
+                nextX + this.ghost.offsetWidth > ghost1.position.x &&
+                nextY < ghost1.position.y + ghost1.ghost.offsetHeight &&
+                nextY + this.ghost.offsetHeight > ghost1.position.y
+            ) {
+                this.setRandomDirection();
+            }
+        } else {
+            this.position.x = nextX;
+            this.position.y = nextY;
+            this.ghost.style.left = this.position.x + "px";
+            this.ghost.style.top = this.position.y + "px";
         }
-    }, 60000);
 
-        if (nextLeft < 0 || nextLeft > window.innerWidth - this.pacman.offsetWidth ||
-            nextTop < 0 || nextTop > window.innerHeight - this.pacman.offsetHeight) {
-            this.direction = null; 
-            return;
-        }
-
-        this.pacman.style.left = nextLeft + "px";
-        this.pacman.style.top = nextTop + "px";
+        requestAnimationFrame(this.move);
     }
 }
 
-const ghost = new Ghost("ghost", { x: 500, y: 300 }, { x: 3, y: 3 });
-// const ghost2 = new Ghost("ghost2", { x: 200, y: 150 }, { x: 4, y: 2 });
+const ghost1 = new Ghost("ghost1", { x: 500, y: 300 }, { x: 2, y: 2 });
+const ghost2 = new Ghost("ghost2", { x: 200, y: 600 }, { x: 3, y: 1 });
