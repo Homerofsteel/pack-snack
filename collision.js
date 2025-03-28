4e1707ec346e59883e5129880ac46f498dd0278b
Timothee <timothee.sylla@ynov.com>
2025-03-28 09:28:59 +0100

    improve-collison

--- collision.js
+++ collision.js
@@ -4,6 +4,7 @@ function collision(Pacman, ghost1, ghost2) {
         Pacman.x + Pacman.width > ghost1.x &&
         Pacman.y < ghost1.y + ghost1.height &&
         Pacman.y + Pacman.height > ghost1.y ||
+        
         Pacman.x < ghost2.x + ghost2.width &&
         Pacman.x + Pacman.width > ghost2.x &&
         Pacman.y < ghost2.y + ghost2.height &&
