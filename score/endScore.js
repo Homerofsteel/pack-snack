244160466cc4d811ff4256d8078d6d2715d4e6f5
Timothee <timothee.sylla@ynov.com>
2025-04-16 09:28:28 +0200

    endScore_fix

--- score/endScore.js
+++ score/endScore.js
@@ -1,3 +1,17 @@
+import score from "./score"
+
 export class EndScore {
-    
+    finalScore;
+    constructor() {
+        this.finalScore = score.value;
+    }
+
+    draw(ctx) {
+        ctx.font = "40px Arial";
+        ctx.fillStyle = "white";
+        ctx.fillText("Final Score: " + this.finalScore, 20, 50);
+    }
+    reset() {
+        score.reset();
+    }
 }
